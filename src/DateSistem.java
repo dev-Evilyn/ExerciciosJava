b67ce578e62e5b4a1d8c9a29ef7369b0a9d12d3d
evilyn <epimentadesousa@gmail.com>
2026-05-14 11:44:57 -0300

    Exercicio01

---
+++ src/DateSistem.java
@@ -0,0 +1,13 @@
+/**
+ * @author Dev-Evilyn
+ */
+
+import java.util.Date;
+
+public class DateSistem {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Hora do sistema é: ");
+        System.out.println(date);
+    }
+}
